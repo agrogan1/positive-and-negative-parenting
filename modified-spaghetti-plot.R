ce2447161342903dd35d9e50470605c5093e3c8d
Andrew Grogan-Kaylor <agrogan@m-m0vy37d2td.local>
2026-03-08 12:41:13 -0400

    added first spaghetti plot and Implications; changed slide level for revealjs

--- modified-spaghetti-plot.R
+++ modified-spaghetti-plot.R
@@ -1,16 +1,39 @@
 library(haven)
 
-reffects <- read_dta("positive-and-negative-parenting/reffects.dta")
+reffects <- read_dta("reffects.dta")
 
 library(ggplot2)
 
+reffects$spankbeta <- .023 + reffects$u4
+
+reffects$spankbetaPOSITIVE <- ifelse(reffects$spankbeta > 0, 
+                                     reffects$spankbeta, 
+                                     NA)
+
+reffects$spankbetaNEGATIVE <- ifelse(reffects$spankbeta <= 0, 
+                                     reffects$spankbeta, 
+                                     NA)
+
+  
 ggplot(reffects,
        aes(color = country)) +
   geom_segment(aes(x = 0,
                    xend = 1,
                    y = 0,
-                   yend = (.023 + u4) * 1)) +
+                   yend = (.023 + u4) * 1,
+                   color = u4 > 0)) +
+  scale_color_manual(values = c("grey", 
+                                "red", 
+                                "grey")) +
+  labs(title = "A 'Slope Only' Spaghetti Plot",
+       subtitle = "Of The Associations of Spanking With Aggression",
+       x = "spanking",
+       y = "aggression",
+       caption = "red values are statistically significant \ngrey values are not") +
   theme_minimal() +
   theme(legend.position = "none")
+
+ggsave("modified-spaghetti-plot.png",
+       height = 3)
 
 
